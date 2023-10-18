ee357f1dd6d81ec8b8f0aac3ccc70a27f8a8f524
Gabriel Couto <gabrielcouto26@gmail.com>
2023-10-17 21:02:18 -0300

    implementa botão de Adicionar

--- src/base/components/BaseTable.vue
+++ src/base/components/BaseTable.vue
@@ -2,37 +2,51 @@
 
 const props = defineProps({
   headers: Array,
-  items: Array
+  items: Array,
+  canAdd: Boolean,
+  add: Function
 })
 
 
 </script>
 
 <template>
-  <v-table class="base-data-table">
-    <thead>
-      <tr>
-        <th 
-          v-for="(item, i) in props.headers"
+  <v-container>
+    <div 
+      v-if="canAdd"
+      class="d-flex flex-row justify-end">
+      <v-btn 
+        color="primary"
+        @click="add">
+        + Adicionar
+      </v-btn>
+    </div>
+
+    <v-table class="base-data-table">
+      <thead>
+        <tr>
+          <th 
+            v-for="(item, i) in props.headers"
+            :key="i"
+          >
+            {{  item.text }}
+          </th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr
+          v-for="(item, i) in props.items"
           :key="i"
         >
-          {{  item.text }}
-        </th>
-      </tr>
-    </thead>
-
-    <tbody>
-      <tr
-        v-for="(item, i) in props.items"
-        :key="i"
-      >
-        <td
-          v-for="header in props.headers"
-          :key="header.value"
-        >
-          {{ item[header.value] }}
-        </td>
-      </tr>
-    </tbody>
-  </v-table>
+          <td
+            v-for="header in props.headers"
+            :key="header.value"
+          >
+            {{ item[header.value] }}
+          </td>
+        </tr>
+      </tbody>
+    </v-table>
+  </v-container>
 </template>
