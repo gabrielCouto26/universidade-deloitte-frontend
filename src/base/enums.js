fe0c95bcc63a13db2fa4466ec11f689e8cfd3126
Gabriel Couto <gabrielcouto26@gmail.com>
2023-10-21 21:56:04 -0300

    implementa nav bar

--- src/base/enums.js
+++ src/base/enums.js
@@ -2,8 +2,27 @@ const STUDENT_USER_TYPE = 1
 const TEACHER_USER_TYPE = 2
 const COORDINATOR_USER_TYPE = 3
 
+const NAV_BAR_ITEMS = [
+    {
+        name: 'Disciplinas',
+        permission_name: 'Discipline',
+        route: '/'
+    },
+    {
+        name: 'Professores',
+        permission_name: 'Teacher',
+        route: '/teacher/new'
+    },
+    {
+        name: 'Alunos',
+        permission_name: 'Student',
+        route: '/student/new'
+    },
+]
+
 export {
     STUDENT_USER_TYPE,
     TEACHER_USER_TYPE,
-    COORDINATOR_USER_TYPE
+    COORDINATOR_USER_TYPE,
+    NAV_BAR_ITEMS
 }
--- src/base/layouts/default/AppBar.vue
+++ src/base/layouts/default/AppBar.vue
@@ -3,28 +3,74 @@ import { logout } from '@/auth/services'
 import { ref } from 'vue';
 import { watch } from 'vue';
 import { useRouter } from 'vue-router'
+import { NAV_BAR_ITEMS } from '@/base/enums'
 
 const router = useRouter()
-const showLogout = ref(true)
+const isLogedIn = ref(true)
+const navBarItems = ref([])
+const userResourcePermissions = ref([])
 
 function handleLogout() {
   logout()
+  userResourcePermissions.value = []
+  navBarItems.value = []
   router.push('/login')
 }
 
+function handleHome() {
+  router.push('/')
+}
+
+function handleRedirect(route) {
+  router.push(route)
+}
+
+function handleNavBar() {
+  const authUser = JSON.parse(localStorage.getItem('user'))
+  if (!authUser)
+    return
+
+  for (let permission of authUser.permissions) {
+    if (permission.can_read)
+      userResourcePermissions.value.push(permission.resource)
+  }
+  
+  navBarItems.value = NAV_BAR_ITEMS.filter(item => {
+    const hasPermission = userResourcePermissions.value.includes(item.permission_name)
+    if (hasPermission) {
+      return {
+        name: item.name,
+        route: item.route
+      }
+    }
+  })
+}
+
 watch(router.currentRoute, () => {
-  showLogout.value = router.currentRoute.value.href !== '/login'
+  isLogedIn.value = router.currentRoute.value.href !== '/login'
+  if(isLogedIn.value)
+    handleNavBar()
 })
 
 </script>
 
 <template>
   <v-app-bar flat class="background-green">
-    <v-app-bar-title class="font-weight-bold">
+    <v-app-bar-title class="font-weight-bold mouseover" @click="handleHome">
       Universidade Deloitte
     </v-app-bar-title>
 
-    <v-btn v-if="showLogout" @click="handleLogout">
+    <div
+      v-if="isLogedIn"
+      v-for="item in navBarItems"
+      @click="handleRedirect(item.route)"
+      :key="item.name"
+      class="mr-5 mouseover"
+      >
+      {{ item.name }}
+    </div>
+
+    <v-btn v-if="isLogedIn" @click="handleLogout">
       Sair
     </v-btn>
   </v-app-bar>
@@ -34,5 +80,9 @@ watch(router.currentRoute, () => {
 .background-green {
   background-color: #006600 !important;
   color: white !important;
+}
+.mouseover:hover {
+  cursor: pointer;
+  font-weight: bold;
 }
 </style>
